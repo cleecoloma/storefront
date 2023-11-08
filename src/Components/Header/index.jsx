0bafad2a2470f64304b560c4e0f06f873351b55f
Chester Lee Coloma <cleecoloma@gmail.com>
2023-11-07 20:17:18 -0800

    added shopping bag icon

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import './Header.css';
 
 function Header() {
   return (
     <AppBar position='static'>
-      <Toolbar>
+      <Toolbar className='toolbar-container'>
         <Typography variant='h6' component='div'>
           KOKO DEPOT
         </Typography>
+        <ShoppingBagIcon fontSize='large' />
       </Toolbar>
     </AppBar>
   );
